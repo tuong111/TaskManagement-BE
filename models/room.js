ee38a1c77027327ae10bb73ad3f4a5227e6ffff5
jest <tuongtf@gmail.com>
2022-06-06 11:06:08 +0700

    room model

--- models/room.js
+++ models/room.js
@@ -1,6 +1,25 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema
+import mongoose from "mongoose";
+const RoomSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    maxPeople: {
+      type: Number,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    roomNumbers: [{ number: Number, unavailableDates: {type: [Date]}}],
+  },
+  { timestamps: true }
+);
 
-const RoomSchema = new Schema({
-    
-})
+export default mongoose.model("Room", RoomSchema);
